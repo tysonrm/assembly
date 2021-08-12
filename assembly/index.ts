d671fc51ad1895e8c174f7bec15c2bdf5f1997b0
tyson <tyson7757@comcast.net>
2021-08-12 00:55:38 -0500

    was

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,3 +3,13 @@
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
+
+export function addWasmByExample(inputString: string): string {
+  return inputString + "Wasm By Example";
+}
+
+export const modelName: string = "wasmmodel";
+export const endpoint: string = "wasmmodels"
+
+
+
